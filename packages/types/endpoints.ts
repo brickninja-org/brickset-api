eefe16147aca83f13327dfd775043527eeb0c39d
brickninja-pro <dev@brick.ninja>
2024-09-09 19:37:27 +0200

    fix(types): update endpoint type getSets

--- packages/types/endpoints.ts
+++ packages/types/endpoints.ts
@@ -10,16 +10,6 @@ export type KnownUnauthorizedEndpoint =
 
 export type KnownEndpoint = KnownAuthenticatedEndpoint | KnownUnauthorizedEndpoint;
 
-// helper types for parameters
-type CombineParameters<P1 extends string, P2 extends string> = `${P1}&${P2}` | `${P2}&${P1}`;
-
-type WithParameters<Url extends string, Parameters extends string | undefined> =
-  Parameters extends undefined ? Url : `${Url}?${Parameters}`;
-
-// helper for endpoints with parameters
-type ParamsParameter = `params=${string}`;
-type EndpointWithParams<Endpoint extends KnownEndpoint> = WithParameters<Endpoint, ParamsParameter>;
-
 // options
 type Options = {};
 
@@ -37,14 +27,9 @@ export type OptionsByEndpoint<Endpoint extends string> =
   Endpoint extends '/api/v3.asmx/getYears' ? Options & { theme?: string } & ApiKeyOptions :
   Partial<AuthenticatedOptions & ApiKeyOptions>;
 
-type ApiResponse<T> = {
-  status: string;
-  message?: string;
-} & T;
-
 // result type for endpoint
 export type EndpointType<Url extends KnownEndpoint | (string & {})> =
-  Url extends '/api/v3.asmx/getSets' ? { status: 'success' | 'error'; message?: string; sets: GetSets } :
+  Url extends '/api/v3.asmx/getSets' ? { status: 'error'; message: string } | { status: 'success'; matches: number; sets: GetSets[] } :
   unknown;
 
 export type ValidateEndpointUrl<T extends string> = unknown extends EndpointType<T> ? 'unknown endpoint url' : T;
