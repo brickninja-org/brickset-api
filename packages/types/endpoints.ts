477547ce2df01bd92872f78e8a181cf0db7f8cb6
brickninja-pro <dev@brick.ninja>
2024-09-10 19:57:19 +0200

    fix(types): update endpoints

--- packages/types/endpoints.ts
+++ packages/types/endpoints.ts
@@ -5,10 +5,18 @@ export type KnownAuthenticatedEndpoint =
 
 export type KnownUnauthorizedEndpoint =
   | '/api/v3.asmx/getSets'
-  | '/api/v3.asmx/getThemes'
-  | '/api/v3.asmx/getYears';
+  | '/api/v3.asmx/getThemes';
 
 export type KnownEndpoint = KnownAuthenticatedEndpoint | KnownUnauthorizedEndpoint;
+
+// helper types for parameters
+type CombineParameters<P1 extends string, P2 extends string> = `${P1}&${P2}` | `${P2}&${P1}`;
+
+type WithParameters<Url extends string, Parameters extends string | undefined = undefined> =
+  Parameters extends undefined ? Url : `${Url}?${Parameters}`;
+
+type UrlWithParams<Url extends KnownEndpoint> =
+  | WithParameters<Url, `params=${string}`>;
 
 // options
 type Options = {};
@@ -17,19 +25,24 @@ export type ApiKeyOptions = {
   apiKey: string;
 };
 
-export type AuthenticatedOptions = ApiKeyOptions &{
+export type AuthenticatedOptions = ApiKeyOptions & {
   userHash: string;
 };
 
 export type OptionsByEndpoint<Endpoint extends string> =
-  Endpoint extends '/api/v3.asmx/getSets' ? Options & { params: string } & ApiKeyOptions :
+  Endpoint extends UrlWithParams<'/api/v3.asmx/getSets'> ? Options & ApiKeyOptions :
   Endpoint extends '/api/v3.asmx/getThemes' ? Options & ApiKeyOptions :
-  Endpoint extends '/api/v3.asmx/getYears' ? Options & { theme?: string } & ApiKeyOptions :
   Partial<AuthenticatedOptions & ApiKeyOptions>;
+
+// Common Brickset API v3 response
+export type ApiResponse<T> = { status: 'success' } & T | { status: 'error'; message: string };
+
+// Brickset API v3 
 
 // result type for endpoint
 export type EndpointType<Url extends KnownEndpoint | (string & {})> =
-  Url extends '/api/v3.asmx/getSets' ? { status: 'error'; message: string } | { status: 'success'; matches: number; sets: GetSets[] } :
+  Url extends UrlWithParams<'/api/v3.asmx/getSets'> ? ApiResponse<{ matches: number; sets: GetSets[] }> :
+  Url extends '/api/v3.asmx/getThemes' ? ApiResponse<{ matches: number, themes: string[] }> :
   unknown;
 
 export type ValidateEndpointUrl<T extends string> = unknown extends EndpointType<T> ? 'unknown endpoint url' : T;
